f7d121470e8d449817185250e7c16b01a41c7918
Sagar <Sagar@LAPTOP-AJO3KB82>
2016-10-23 10:46:56 +0530

    Connected to MySQL

---
+++ src/main/resources/data/mysql/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `Greeting`;
+
+CREATE TABLE Greeting ( 
+	`id` bigint(20) unsigned NOT NULL auto_increment,  	
+	`text` varchar(100) NOT NULL,	
+  	PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
